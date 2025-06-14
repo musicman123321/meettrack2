f1ecc7470117f9fc7cd87a61176d1bd0cfd99cf2
Aidan Rabinowitz <aidanrabinowitz@gmail.com>
2025-06-14 18:58:28 +0200

    Hopefully fixed first load working

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,5 @@
-import path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import { tempo } from "tempo-devtools/dist/vite";
-
-const conditionalPlugins: [string, Record<string, any>][] = [];
-
-// @ts-ignore
-if (process.env.TEMPO === "true") {
-  conditionalPlugins.push(["tempo-devtools/swc", {}]);
-}
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "development" ? "/" : "./",
+  base: "/", // Set this to absolute root
 
   plugins: [
     react({
@@ -20,20 +7,29 @@ export default defineConfig({
     }),
     tempo(),
   ],
+
   resolve: {
     preserveSymlinks: true,
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
   server: {
     // @ts-ignore
     allowedHosts: true,
+    historyApiFallback: true, // Add this
   },
+
   build: {
+    outDir: "dist",
+    emptyOutDir: true,
     rollupOptions: {
       output: {
         manualChunks: undefined,
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`,
       },
     },
   },
